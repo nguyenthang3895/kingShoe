cec4e6b24fcc3427b88f3179e09db8ae0ea70d51
nguyenthang3895 <nguyenthang030895@gmail.com>
2022-07-24 16:09:01 +0700

    up usestate

--- src/Components/ListCard/index.js
+++ src/Components/ListCard/index.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 export default function ListCard(props) {
+  const [like, setLike] = useState(0);
+  const handleLike = () => {
+    setLike(like + 1);
+  };
   const listCard = props.listcard.map((card, index) => {
     return (
       <div className="card-container" key={index}>
@@ -7,6 +13,12 @@ export default function ListCard(props) {
         </div>
         <div className="card-title">{card.title}</div>
         <div className="card-description">{card.description}</div>
+        <div className="d-flex justify-content-center">
+          <div className="px-3" onClick={handleLike} key={index}>
+            <i class="fa fa-heart" aria-hidden="true"></i>
+          </div>
+          <span>{like}</span>
+        </div>
       </div>
     );
   });
